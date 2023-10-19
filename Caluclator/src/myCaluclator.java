1c9b767fadd9eab67f340430f22a25ed62234ece
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 05:39:44 +0530

    commit

---
+++ Caluclator/src/myCaluclator.java
@@ -0,0 +1,21 @@
+
+public class myCaluclator {
+
+	public static void main(String[] args) {
+	// perform addition
+		public class additon(int a,int b)
+		{
+			//return the value;
+		}
+		public class subtraction(int a,int b)
+		{
+			//return the result
+		}
+		public class multiplicaton(int a,int b)
+		{
+			//return the result
+		}
+	}
+	//Write the Main functionS
+
+}
